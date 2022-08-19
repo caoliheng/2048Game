ae1aa09ed6c937d5a03c8fadd8ae59f55aa75446
Liheng Cao <lihengcao@gmail.com>
2022-08-18 23:10:39 -0700

    added some more template code

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 from enum import IntEnum
 
 
-class Move(IntEnum):
+class Direction(IntEnum):
     UP = 1
     DOWN = 2
     LEFT = 3
@@ -9,8 +9,8 @@ class Move(IntEnum):
 
 
 class Game:
-    def __init__(self, board=None) -> None:
-        if not board:
+    def __init__(self, board=None, generation=1) -> None:
+        if not board:  # use default board
             self.board = [
                 [2, 0, 0, 0],
                 [0, 0, 0, 0],
@@ -18,17 +18,28 @@ class Game:
                 [0, 0, 0, 0],
             ]
         else:
+            # assume that input is in my format
+            # TODO convert 1D (length 16) board into 2D board (4x4)
             self.board = board
 
-    def move(direction: Move) -> None:
+        self.generation = generation
+
+    def move(direction: Direction) -> None:
+        pass
+
+    def generate(self) -> None:
+        # generate {self.generation} number of values (for now, just the value 2) into random empty (value 0) spaces
         pass
 
     def __str__(self) -> str:
+        # str is "informal" representation
         pass
 
     def __repr__(self) -> str:
+        # repr is "formal" representation
         pass
 
 
 if __name__ == "__main__":
+    # TODO generate random board
     pass
