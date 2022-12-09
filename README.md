d554595520e98900b9dd52f6ca806fe66ddfa767
Liheng Cao <lihengcao@gmail.com>
2022-12-08 23:23:40 -0800

    worked on it a bit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 2048Game
 
-simple 2048 game
+Simple CLI implementation of the 2048 game
+This was an interview question I got at a certain company. I thought it's interesting enough to do as a side project.
--- game.py
+++ game.py
@@ -1,15 +1,16 @@
-from enum import IntEnum
+# interview question from a certain company...
+from random import randint
 
 
-class Direction(IntEnum):
-    UP = 1
-    DOWN = 2
-    LEFT = 3
-    RIGHT = 4
+# str direction to int di, dj (like dx, dy, but accounting for matrix indexing direction)
+direction: dict[str, tuple[int, int]] = {"up": (-1, 0), "down": (1, 0), "left": (0, -1), "right": (0, 1)}
 
 
 class Game:
-    def __init__(self, board=None, generation=1) -> None:
+    NEW_TILES_PER_TURN = 1
+    NEW_TILE_VALUE = 2  # TODO make new tile value variable
+
+    def __init__(self, board=None) -> None:
         if not board:  # use default board
             self.board = [
                 [2, 0, 0, 0],
@@ -19,32 +20,63 @@ class Game:
             ]
         else:
             # assume that input is in my format
-            # TODO convert 1D (length 16) board into 2D board (4x4)
+            # TODO error check board and convert if necessary
             self.board = board
 
-        self.generation = generation  # number of new numbers to add on each move
 
-    def move(direction: Direction) -> None:
+    def move(direction: str) -> None:
+        """
+        Given a str direction, updates the game state.
+        """
+        # TODO
         pass
 
+    
     def generate(self) -> None:
-        # generate {self.generation} number of values (for now, just the value 2) into random empty (value 0) spaces
-        zeros = 0 
-        for r in self.board:
-            for c in r:
-                zeros += c == 0
+        """
+        Generates self.NEW_TILES_PER_TURN values into random empty slots
+        """
+        zeros = sum(1 for c in r for r in self.board if c == 0)
 
-        
-        pass
+        location = randint(zeros)
+
+        # TODO generate more than one random value
+        # TODO is there a better way to do this?
+        # for _ in range(self.NEW_TILES_PER_TURN):
+
+        for r in len(self.board):
+            for c in len(self.board[0]):
+                if location == 0:
+                    self.board[r][c] = self.NEW_TILE_VALUE
+                    return
+                location -= 1
+
 
     def __str__(self) -> str:
-        # str is "informal" representation, whatever that means
-        pass
-
-    def __repr__(self) -> str:
-        # repr is "formal" representation, whatever that means
+        """str is "informal" representation, whatever that means"""
+        # TODO 
         return f""
 
 
+    def __repr__(self) -> str:
+        """repr is "formal" representation, whatever that means"""
+        # TODO 
+        return f""
+
+
+    
+    def play(self):
+        """
+        display game state to stdout, get move from stdin, update game state, repeat
+        """
+        # TODO 
+        print("Use 'quit' to quit. Valid commands are 'up', 'down', 'left', and 'right'.")
+        while command := input() != "quit":
+            print("zzz")
+
+
+
 if __name__ == "__main__":
-    pass
+    g = Game()
+
+    g.play()
