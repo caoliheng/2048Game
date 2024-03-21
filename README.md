aa198f5ec1ef4af677689edf04ead093a81c77b6
Liheng Cao <20694415+lihengcao@users.noreply.github.com>
2024-03-21 15:29:43 -0400

    base game completed. did some basic QA

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # 2048Game
-
 Simple CLI implementation of the 2048 game
-This was an interview question I got at a certain company. I thought it's interesting enough to do as a side project.
+This was an interview question I got at a certain company. I thought it was interesting enough to do as a side project.
+
+# How to play
+Clone the repo and `cd` to the folder. Then run: 
+```python3 game.py```
+
+# Todo
+- Make a frontend so that it can be played in the browser. Maybe an opportunity to try out some wasm since this was made in python. 
+- Pretty print with borders and with consistent spacing
+    - If we get 16, the numbers aren't aligned anymore. 
+- Game over screen. No way to lose right now :p
+- Replace output instead of having the game history in the terminal
+- Undo last move 
--- game.py
+++ game.py
@@ -11,48 +11,106 @@ from typing import Optional
 
 class Game:
     """The Game!"""
-    NEW_TILES_PER_TURN = 1
-    NEW_TILE_VALUE = 1  # TODO make new tile value variable
 
-    # str direction to int di, dj (like dx, dy, but accounting for matrix indexing direction)
-    DIRECTIONS: dict[str, tuple[int, int]] = {
-        "up": (-1, 0), "down": (1, 0), "left": (0, -1), "right": (0, 1)
-    }
+    # NEW_TILES_PER_TURN = 1
+    NEW_TILE_VALUE = 1
 
-    # TODO variable board size
-    BOARD_SIZE = 4, 4
+    BOARD_SIZE = 4  #TODO variable board shape and size
 
     def __init__(self, board: Optional[list[list[int]]] = None) -> None:
-        if not board:  # use default board
-            self.board = [[0 for _ in range(Game.BOARD_SIZE[0])]
-                          for _ in range(Game.BOARD_SIZE[1])]
-
-            self.board[0][0] = Game.NEW_TILE_VALUE
-        else:
-            # assume that input is in my format
+        if board:  # assume that input is in my format
             # TODO error check board and convert if necessary
             self.board = board
+        else:
+            # use default board
+            self.board = [
+                [0 for _ in range(Game.BOARD_SIZE)] for _ in range(Game.BOARD_SIZE)
+            ]
 
-    def move(self, direction: str) -> None:
+            self.generate()
+
+        print("Controls: (includes vim keybindings!)")
+        for k, v in Game.CONTROLS.items():
+            print(f"{k}\t: {', '.join(val for val in v)}")
+        print()
+        self.display_board()
+
+    # str direction to int di, dj (like dx, dy, but accounting for matrix indexing direction)
+    DIRECTIONS = {
+        "up": ("c", True),
+        "down": ("c", False),
+        "left": ("r", True),
+        "right": ("r", False),
+    }
+
+    @staticmethod
+    def get_iteration_params(command: str) -> Optional[tuple[str, bool]]:
+        """Convert the command"""
+        for k, v in Game.CONTROLS.items():
+            if command in v:
+                return Game.DIRECTIONS[k]
+
+        print("Invalid command. Try again.")
+        return None
+
+    @staticmethod
+    def resolve_row_col(row_or_col: str, i, j) -> tuple[int, int]:
+        """Sometimes we want to iterate rows first. Other times we want to iterate columns first."""
+        return i if row_or_col == "r" else j, j if row_or_col == "r" else i
+
+    def move(self, command: str) -> None:
         """
-        Given a str direction, updates the game state.
+        Given a command, updates the game state.
         """
-        # TODO
+        iteration = self.get_iteration_params(command)
+
+        if iteration is None:
+            return
+
+        row_or_col, direc = iteration
+
+        a, b, c = (
+            (0 if direc else Game.BOARD_SIZE - 1),
+            (Game.BOARD_SIZE if direc else -1),
+            (1 if direc else -1),
+        )
+
+        for i in range(Game.BOARD_SIZE):
+            stack = []
+            for j in range(a, b, c):
+                x, y = self.resolve_row_col(row_or_col, i, j)
+
+                cur = self.board[x][y]
+                if cur == 0:
+                    continue
+                while stack and stack[-1] == cur:
+                    stack.pop()
+                    cur *= 2
+
+                stack.append(cur)
+
+            stack = stack + [0] * (Game.BOARD_SIZE - len(stack))
+
+            for j, val in zip(range(a, b, c), stack):
+                x, y = self.resolve_row_col(row_or_col, i, j)
+
+                self.board[x][y] = val
+
+        self.generate()
 
     def generate(self) -> None:
         """
-        Generates Game.NEW_TILES_PER_TURN values into random empty slots
+        Generates a value into a random empty slot
         """
-        zeros = sum(1 for row in self.board for cell in self.board if cell == 0)
+        zeros = sum(1 for row in self.board for cell in row if cell == 0)
 
         location = randint(0, zeros)
 
-        # TODO generate more than one random value
-        # TODO is there a better way to do this?
-        # for _ in range(self.NEW_TILES_PER_TURN):
+        for row_ind in range(Game.BOARD_SIZE):
+            for col_ind in range(Game.BOARD_SIZE):
+                if self.board[row_ind][col_ind] != 0:
+                    continue
 
-        for row_ind in range(Game.BOARD_SIZE[0]):
-            for col_ind in range(Game.BOARD_SIZE[1]):
                 if location == 0:
                     self.board[row_ind][col_ind] = self.NEW_TILE_VALUE
                     return
@@ -60,28 +118,29 @@ class Game:
 
     def __str__(self) -> str:
         """str is "informal" representation, whatever that means"""
-        # TODO
-        return ""
+        return "\n".join([" ".join([str(n) for n in row]) for row in self.board])
 
-    def __repr__(self) -> str:
-        """repr is "formal" representation, whatever that means"""
-        # TODO
-        return ""
+    def display_board(self) -> None:
+        """Prints the board output with a starting newline for padding"""
+        print()
+        print(self)
+
+    CONTROLS = {
+        "quit": {"quit", "q"},
+        "up": {"up", "k"},
+        "down": {"down", "j"},
+        "left": {"left", "h"},
+        "right": {"right", "l"},
+    }
 
     def play(self):
         """
         display game state to stdout, get move from stdin, update game state, repeat
         """
-        # TODO
-        print("Use 'quit' to quit. Valid commands are 'up', 'down', 'left', and 'right'.")
-        while (command := input()) != "quit":
-            print(f"{command=}")
+        while (command := input("Move: ")) not in Game.CONTROLS["quit"]:
+            self.move(command)
 
-            match command:
-                case "up":
-                    print("up")
-                case _:
-                    print("anything but up")
+            self.display_board()
 
 
 if __name__ == "__main__":
